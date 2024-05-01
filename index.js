90ddf3e8f39ccf8c9bb1e9140e9ee5fc455f47bd
Youness Najeh <najehy2803@gmail.com>
2024-05-01 04:16:30 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 const express = require('express');
 const app = express()
+
+const cors = require('cors');
+app.use(cors(
+  {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
--- routes/LogIn.js
+++ routes/LogIn.js
@@ -5,9 +5,9 @@ router.use(express.json());
 router.use(express.urlencoded({extended:true}));
 
 router.get("/", async (req, res) => {
-    const { UserName_Employe, Password_Employe } = req.body;
+    const { UserName_Employe, Password_Employe } = req.query;
     if (!UserName_Employe || !Password_Employe){
-        return   res.status(406).send("Il semble que certaines informations soient manquantes ou incorrectes. Veuillez examiner les champs en surbrillance et soumettre à nouveau.");
+        return res.status(406).send("Il semble que certaines informations soient manquantes ou incorrectes. Veuillez examiner les champs en surbrillance et soumettre à nouveau.");
     }
     try {
         const Select_Records = await executeQuery({
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
         });
 
         if (Select_Records.length !== 0) {
-            // Check if Password_Employe matche
+            // Check if Password_Employe matches
             const status = Select_Records.some(record => record.Password_Employe !== Password_Employe);
             if (status) {
                 return res.json({ status: "Password" });
@@ -30,7 +30,6 @@ router.get("/", async (req, res) => {
         console.error("Oops! Nous avons rencontré un problème lors de la récupération de vos données. Merci de réessayer dans quelques instants. Si le problème persiste, contactez notre équipe d'assistance pour obtenir de l'aide.");
         res.status(500).send("Nous rencontrons actuellement des difficultés pour nous connecter au serveur. Veuillez vérifier votre connexion Internet ou réessayer sous peu.");
     }
-
 });
 
 module.exports = router;
