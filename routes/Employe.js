8a0921565106100cd330fb71a64485442fed4a1e
Youness Najeh <122638876+Yona2803@users.noreply.github.com>
2024-05-06 03:39:12 +0100

    Employe.js

--- routes/Employe.js
+++ routes/Employe.js
@@ -5,22 +5,17 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get("/", async (req, res) => {
-    const { Num_Technicien, Num_Physicien } = req.query;
-    if (!Num_Technicien || !Num_Physicien) {
+    const { Num_Employe } = req.query;
+    if (!Num_Employe) {
         return res.status(406).send("Bad Request");
     }
     try {
-        const Technicien = await executeQuery({
+        const Employe_Data = await executeQuery({
             query: "SELECT `Nom_Employe`, `Prenom_Employe`, `Specialite` FROM `employes` WHERE Num_Employe = ?",
-            values: [Num_Technicien],
+            values: [Num_Employe],
         });
 
-        const Physicien = await executeQuery({
-            query: "SELECT `Nom_Employe`, `Prenom_Employe`, `Specialite` FROM `employes` WHERE Num_Employe = ?",
-            values: [Num_Physicien],
-        });
-
-        return res.status(200).json({ Technicien, Physicien });
+        return res.status(200).json({Employe_Data});
     } catch (error) {
         console.error(error);
         res.status(500).send(error);
